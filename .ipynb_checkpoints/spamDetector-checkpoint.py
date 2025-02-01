7c1b833fb750042a33db0a4097d73c80e6c7c0eb
Jericho <jerichopecho123@gmail.com>
2025-02-02 00:31:56 +0800

    initial commit

---
+++ .ipynb_checkpoints/spamDetector-checkpoint.py
@@ -0,0 +1,21 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open("spam.pkl", "rb"))
+cv = pickle.load(open("vectorizer.pkl", "rb"))
+
+def main():
+	st.title("Email Spam Classification")
+	st.subheader("using Naive Bayes")
+	msg=st.text_input("Enter a Text: ")
+	if st.button("Predict"):
+		data = [msg]
+		vect = cv.transform(data).toarray()
+		prediction = model.predict(vect)
+		result = prediction[0]
+		if result == 1:
+			st.error("This is a spam")
+		else:
+			st.success("This is a not a spam")
+
+main()
